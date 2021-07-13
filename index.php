ab8ddf4b896746c100ec86d11804a938915e2af9
Terrance Shaw <terrance.shaw@gmail.com>
2021-07-13 16:45:13 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="/js/jquery.js"></script>
+    <link rel="stylesheet" href="css/fontawesome/css/all.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+    <title>Correspondence Tracking System</title>
+</head>
+<body>
+
+
+
+</body>
+</html>
